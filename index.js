22cf6795e0480a7c98468dadc8ebed06984af82c
Shivam Choudhary <shivamch1704@gmail.com>
2023-10-02 03:06:00 +0530

    cron

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const express = require('express');
 const {scrapeData}= require('./scrapeData'); 
-
+const cors = require('cors');
 const app = express();
 const port = 3000; 
+app.use(cors());
+
+app.get('/cron', async(req, res) => {
+    console.log('cron boi');
+    res.status(200).send('Thanks buddy for keeping me alive...');
+});
+
 app.get('/scrape', async (req, res) => {
     let { url, limit, title } = req.query;
     if(!limit) limit = 5;
-    if(!url && title) url = `https://libgen.is/search.php?req=${encodeURIComponent(title)}&open=3&res=100&view=simple&phrase=1&column=def`;
+    if(!url && title) url = `https://libgen.is/search.php?req=${encodeURIComponent(title)}`;
     try {
         const scrapedData = await scrapeData(url, parseInt(limit));
-        res.json(scrapedData);
+        console.log('successfull scrape');
+        res.status(200).json(scrapedData);
     } catch (error) {
+        console.log('un-successfull scrape');
         res.status(500).json({ error: 'An error occurred while scraping data.' });
     }
 });
